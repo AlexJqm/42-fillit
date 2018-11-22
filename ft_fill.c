bd5d94e6a77f63738c1aef8d605dc651f7dc8a85
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2018-11-22 22:56:38 +0100

    22-11-2018

--- ft_fill.c
+++ ft_fill.c
@@ -16,24 +16,24 @@
 /*
 **	fill a tab of pieces
 */
-t_piece		**ft_fill(char *file, int nb_pcs)
+t_piece		**ft_fillit(char *file, int nb_pcs)
 {
-	t_piece	*num_pc[nb_pcs + 1];
+	t_piece		*num_pc[nb_pcs + 1];
 	int		fd;
-	int 	i;
-	int 	j;
+	int		i;
+	int		j;
 
 	tab[nb_pcs] = NULL;
 	i = 0;
 	j = 0;
 	if ((fd = open(file, O_RDONLY)) <= 0)
-		ft_printerror(fd, 1);
+		ft_print_error(fd, 1);
 	while (j < nb_pcs)
 	{
 		while (i < 4)
 		{
 			if (get_next_line(fd, &(num_pc[j]->piece[i])) != 1)
-				ft_printerror(fd, 1);
+				ft_print_error(fd, 1);
 			i++;
 		}
 		j++;
