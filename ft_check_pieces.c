c68cbdad8612c37fda63f893c350b99fa62fd5a4
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2018-12-05 18:04:17 +0100

    05-12-2018

---
+++ ft_check_pieces.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_pieces.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aljacque <aljacque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 15:09:32 by aljacque          #+#    #+#             */
+/*   Updated: 2018/12/05 17:47:21 by aljacque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static	int  ft_check_square(char **tab)
+{
+	int i;
+	int j;
+
+	i = -1;
+	j = 0;
+	while (j < 3)
+	{
+		while (++i < 3)
+			if (tab[j][i] == '#')
+				if (tab[j][i + 1] == '#')
+					if (tab[j + 1][i] == '#')
+						(tab[j + 1][i + 1] == '#') ? return (1) : return (0);
+				else
+					return (0);
+		i = -1;
+		j++;
+	}
+	return (0);
+}
+
+static	int ft_check_link(char **tab, int j, int i) /* A voir */
+{
+	int link;
+
+	link = 0;
+	if (j != 0)
+		if (tab[j - 1][i] == '#')
+			link++;
+	if (j != 3)
+		if (tab [j + 1][i] == '#')
+			link++;
+	if (i != 0)
+		if (tab[j][i - 1] == '#')
+			link++;
+	if (i != 3)
+		if (tab[j][i + 1] == '#')
+			link++;
+	return (link);
+}
+
+static  int ft_count_elem(char **tab)
+{
+	int i;
+	int j;
+	int elem;
+
+	i = 0;
+	j = -1;
+	elem = 0;
+	while (++j < 4)
+	{
+		while (i < 4)
+			if (tab[j][i++] == '#')
+				elem++;
+		i = 0;
+	}
+	if (elem == 4)
+		return (1);
+	return (0);
+}
+
+static  void  ft_check_piece(char **tab)
+{
+	int i;
+	int j;
+	int link;
+
+	i = 0;
+	j = -1;
+	link = 0;
+	if (ft_check_square(tab))
+		return (1);
+	while (++j < 4)
+	{
+		while (i < 4)
+			if (tab[j][i++] == '#')
+				link += ft_check_link(tab, j, i); /* A voir */
+		i = 0;
+	}
+	if (link == 6)
+		return (1);
+	return (0);
+}
+
+void         ft_check_pieces(t_piece *tab, int fd)
+{
+	int i;
+
+	i = -1;
+	while (tab[++i].piece[0][0])
+	{
+		if (!(ft_check_piece(tab[i].piece)))
+			ft_print_error(fd, 1);
+		if (!(ft_count_elem(tab[i].piece)))
+			ft_print_error(fd, 1);
+	}
+}
