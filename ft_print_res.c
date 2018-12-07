c496853369759c42fc70e8eb20bf51e4dd2a90ed
C1em <martin.corentin98@gmail.com>
2018-12-07 16:07:36 +0100

    07-12-2018

--- ft_print_res.c
+++ ft_print_res.c
@@ -6,12 +6,13 @@
 /*   By: coremart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 15:09:14 by coremart          #+#    #+#             */
-/*   Updated: 2018/12/07 15:56:43 by coremart         ###   ########.fr       */
+/*   Updated: 2018/12/07 16:05:10 by coremart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/fillit.h"
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_print_res(const char **tab, size_t size)
 {
@@ -26,6 +27,7 @@ void	ft_print_res(const char **tab, size_t size)
 	{
 		while (j < size)
 		{
+			printf("j :%zu\n", j);
 			if (tab[i][j])
 				write(1, &tab[i][j], 1);
 			else
