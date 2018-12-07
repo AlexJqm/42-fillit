7b9c05d1db8ee5920b6f98d72a7237c2f5e47ecb
C1em <coremart@e2r4p4.42.fr>
2018-12-07 14:28:15 +0100

    07-12-2018

--- ft_fillit.c
+++ ft_fillit.c
@@ -6,7 +6,7 @@
 /*   By: coremart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 16:07:48 by coremart          #+#    #+#             */
-/*   Updated: 2018/12/06 17:36:56 by coremart         ###   ########.fr       */
+/*   Updated: 2018/12/07 14:27:55 by coremart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,51 @@
 #include "includes/fillit.h"
 #include <stdlib.h>
 
-static int	is_fill(t_piece *tab, char **res, size_t size, int pos)
+static char		**fill_with(char piece[4][4], const char **tab, size_t size, size_t pos)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	char **res;
+
+	i = pos / 4;
+	j = pos % 4;
+	k = 0;
+	l = 0;
+	if (!(res = ft_tabdup(tab, size)))
+		return (NULL);
+	while (k < 4)
+	{
+		while (l < 4)
+		{
+			if (piece[k][l] != '.')
+			{
+				if (i + k >= size || j + l >= size)
+				{
+					ft_free_tab(res);
+					return (NULL);
+				}
+				res[i + k][j + l] = piece[k][l];
+			}
+			l++;
+		}
+		l = 0;
+		k++;
+	}
+	return (res);
+}
+
+static int		is_fill(t_piece *tab, char **res, size_t size, size_t pos)
 {
 	if (!tab.piece[0][0])
+	{
+		ft_print_res(res, size);
 		return (1);
-	if (!fill_with(tab.piece, res, size, pos))
-		return (is_fill(tab + 1, res, size, pos));
-	else
-		return (is_fill(tab, res, size, pos));
-	if (pos == size * size)
-	else if (pos = size * size)
+	}
+	if (!res || pos > size * size)
 		return (0);
-	return (is_fill(tab, res, size, pos + 1));
+	return (is_fill(tab + 1, fill_with(tab.piece, res, size, pos), size, 0) || is_fill(tab, res, size, pos + 1))
 }
 
 char		**ft_fillit(t_piece *tab)
