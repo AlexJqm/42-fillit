07d4facb1e00d0c551bd003c6743ffa6907c5b22
C1em <martin.corentin98@gmail.com>
2018-12-08 14:19:47 +0100

    rm

--- compile.sh
+++
@@ -1,3 +0,0 @@
-clang -Wall -Wextra -Werror -I libft/includes -o ft_fillit.o -c ft_fillit.c
-clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
-clang -o fillit main.o ft_fillit.o -I libft/includes -L libft/ -lft
